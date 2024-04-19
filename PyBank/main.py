5c13109f3142eae9215bc86f1d7a107c2c663746
Paul M <paul.magarill@gmail.com>
2024-04-18 22:02:32 -0700

    final

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,22 +1,30 @@
+# Print output header
 print("Financial Analysis")
 print("--------------------")
 
+# Import dependencies
 import os
 import csv
 
+# Establish filepath for CSV input file
 csvpath = os.path.join("Resources", "budget_data.csv")
 
+# Open and read CSV file
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+
+    # Read the header row first
     csv_header = next(csvreader)
 
+    # Define variables
     month_count = 0
     total = 0
     change = 0
     changes = []
     previous_profit = 0
     change_month = []
-
+    
+    # Calculate total months, total profit, and average change in profit
     for row in csvreader:
         profit = int(row[1])
         month = row[0]
@@ -29,16 +37,37 @@ with open(csvpath) as csvfile:
             change_month.append(str(month))
 
         previous_profit = profit
+    
+    average = round(sum(changes) / len(changes), 2)
+
+    # Print results of total months, total profit, and average change in profit
     print(f'Total Months: {month_count}')
     print(f'Total: ${total}')
-    average = round(sum(changes) / len(changes), 2)
     print(f'Average Change: ${average}')
 
+    # Define change variables
     max_change = max(changes)
     min_change = min(changes)
 
+    # Calculate min and max monthly changes
     max_change_month = change_month[changes.index(max_change)]
     min_change_month = change_month[changes.index(min_change)]
   
+    # Print results of min and max montly changes
     print(f'Greatest Increase in Profits: {max_change_month} (${max_change})')
     print(f'Greatest Decrease in Profits: {min_change_month} (${min_change})')
+
+# Establish filepath for txt output file
+output_path = os.path.join("Analysis", "PyBank_results.txt")
+
+# Open txt file in "write" mode
+with open(output_path, 'w') as txtfile:
+
+    # Write results into txt file
+    txtfile.write("Financial Analysis"'\n')
+    txtfile.write("--------------------"'\n')
+    txtfile.write(f'Total Months: {month_count}''\n')
+    txtfile.write(f'Total: ${total}''\n')
+    txtfile.write(f'Average Change: ${average}''\n')
+    txtfile.write(f'Greatest Increase in Profits: {max_change_month} (${max_change})''\n')
+    txtfile.write(f'Greatest Decrease in Profits: {min_change_month} (${min_change})')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,19 +1,27 @@
-print("Financial Analysis")
+# Print output header
+print("Election Results")
 print("--------------------")
 
+#Import dependencies
 import os
 import csv
 
+# Establish filepath for CSV input file
 csvpath = os.path.join("Resources", "election_data.csv")
 
+# Open and read CSV file
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    
+    # Read the header row first
     csv_header = next(csvreader)
 
+    #Define variables
     total_vote_count = 0
     candidates = []
     candidate_votes = {}
 
+    # Calculate total votes
     for row in csvreader:
         total_vote_count += 1
 
@@ -24,19 +32,43 @@ with open(csvpath) as csvfile:
             candidate_votes[candidate] = 0
         candidate_votes[candidate] += 1
     
+    # Print total votes
     print(f'Total Votes: {total_vote_count}')
     print("--------------------")
 
+    # Calculate votes and percentages by candidate
     for candidate in candidates:
-        percentage = round(candidate_votes[candidate] / total_vote_count * 100, 2)
-        print(f"{candidate}: {percentage}% ({candidate_votes[candidate]})")
+        percentage = round(candidate_votes[candidate] / total_vote_count * 100, 3)
+        
+        # Print votes and percentages by candidate
+        print(f'{candidate}: {percentage}% ({candidate_votes[candidate]})')
 
     print("--------------------")
 
+    # Define max vote variable
     max_votes = max(candidate_votes.values())
 
-    for candidate, candidate_votes in candidate_votes.items():
-        if candidate_votes == max_votes:
-            winning_candidate = candidate
+    # Determine winner
+    for candidate, winning_candidate_votes in candidate_votes.items():
+        if winning_candidate_votes == max_votes:
+            winner = candidate
     
-    print(f'Winner: {winning_candidate}')
+    # Print winner
+    print(f'Winner: {winner}')
+
+# Establish filepath for txt output file
+output_path = os.path.join("Analysis", "PyPoll_results.txt")
+
+# Open txt file in "write" mode
+with open(output_path, 'w') as txtfile:
+
+    # Write results into txt file
+    txtfile.write("Election Results"'\n')
+    txtfile.write("--------------------"'\n')
+    txtfile.write(f'Total Votes: {total_vote_count}''\n')
+    txtfile.write("--------------------"'\n')
+    for candidate in candidates:
+        percentage = round(candidate_votes[candidate] / total_vote_count * 100, 3)
+        txtfile.write(f'{candidate}: {percentage}% ({candidate_votes[candidate]})''\n')
+    txtfile.write("--------------------"'\n')
+    txtfile.write(f'Winner: {winner}')
